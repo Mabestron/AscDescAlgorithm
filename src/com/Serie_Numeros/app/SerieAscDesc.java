fd30ca33f04ea029972dc9f26dc3dacae8d86da6
Mabestron <ivangamaliel555@hotmail.com>
2019-11-15 23:50:14 -0600

    AscDesc

---
+++ src/com/Serie_Numeros/app/SerieAscDesc.java
@@ -0,0 +1,21 @@
+package com.Serie_Numeros.app;
+
+public class SerieAscDesc 
+{
+	public static void main(String[]args)
+	{
+		//Ascendente
+		for(int i=0; i<=100;i++)
+		{
+			System.out.println(i);
+		}
+		
+		//Descendente
+		for(int i=100; i>=0;i--)
+		{
+			System.out.println(i);
+		}
+		
+		
+	}
+}
